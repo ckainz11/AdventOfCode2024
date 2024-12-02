a32d27a9f0b5a0f6995aab0e90dcb9577dc72be8
ckainz11 <christoph.kainz11@gmail.com>
2024-12-02 09:31:43 +0100

    solve day 2

---
+++ src/main/kotlin/days/day02/Day2.kt
@@ -0,0 +1,17 @@
+package days.day02
+
+import days.Day
+import util.allInts
+import kotlin.math.abs
+
+class Day2(override val input: String) : Day<Int>(input) {
+    private val reports = input.lines().map { it.allInts() }
+    private fun safe(report: List<Int>) = report.zipWithNext().all { (a, b) -> abs(a - b) in 1..3 } &&
+            (report.sorted() == report || report.sorted().reversed() == report)
+
+    override fun solve1(): Int = reports.count { safe(it) }
+
+    override fun solve2(): Int = reports.count { safe(it) || (0..it.size).any { index ->
+        safe(it.take(index) + it.drop(index + 1))
+    } }
+}
--- src/main/kotlin/util/DayFactory.kt
+++ src/main/kotlin/util/DayFactory.kt
@@ -2,12 +2,14 @@ package util
 
 import days.Day
 import days.day01.Day1
+import days.day02.Day2
 
 
 class DayFactory {
     companion object {
         val days = mapOf(
             1 to ::Day1,
+            2 to ::Day2
         )
 
         fun getDayObject(day: Int, input: String): Day<out Any> {
