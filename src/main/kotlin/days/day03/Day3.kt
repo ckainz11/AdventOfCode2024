f30bc215ec9c2223489ca66e8801d128a590c796
ckainz11 <christoph.kainz11@gmail.com>
2024-12-03 11:47:51 +0100

    refactor day 3

--- src/main/kotlin/days/day03/Day3.kt
+++ src/main/kotlin/days/day03/Day3.kt
@@ -5,11 +5,11 @@ import util.allInts
 
 class Day3(override val input: String) : Day<Int>(input) {
 
-	private val multiRegex = "mul\\(\\d+,\\d+\\)".toRegex()
-	private val doRegex = "(don't\\(\\)|do\\(\\))".toRegex()
+	private val multiplicationRegex = "mul\\(\\d+,\\d+\\)".toRegex()
+	private val instructionRegex = "(don't\\(\\)|do\\(\\))".toRegex()
 
-	private val multiplications = multiRegex.findAll(input).map { match -> Multi.from(match) }
-	private val instructions = doRegex.findAll(input).map { match -> Instruction.from(match) }
+	private val multiplications = multiplicationRegex.findAll(input).map { match -> Multiplication.from(match) }
+	private val instructions = instructionRegex.findAll(input).map { match -> Instruction.from(match) }
 
 	override fun solve1(): Int = multiplications.sumOf { it.result }
 	override fun solve2(): Int = multiplications.sumOf { if (it.canRunWith(instructions)) it.result else 0 }
@@ -22,17 +22,17 @@ class Day3(override val input: String) : Day<Int>(input) {
 
 		}
 
-		fun contains(multi: Multi) = multi.start > start
+		fun contains(multiplication: Multiplication) = multiplication.start > start
 
 	}
 
-	data class Multi(val start: Int, val result: Int) {
+	data class Multiplication(val start: Int, val result: Int) {
 
 		fun canRunWith(instructions: Sequence<Instruction>) = instructions.findLast { it.contains(this) }?.doo ?: true // first is always "do"
 
 		companion object {
 
-			fun from(match: MatchResult) = Multi(match.range.first, match.value.allInts().let { (a, b) -> a * b })
+			fun from(match: MatchResult) = Multiplication(match.range.first, match.value.allInts().let { (a, b) -> a * b })
 
 		}
 	}
