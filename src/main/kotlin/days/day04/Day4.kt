1a58b145e71faa25bce04a1cc597d8893a03cf63
ckainz11 <christoph.kainz11@gmail.com>
2024-12-04 12:52:48 +0100

    refactor day 4

--- src/main/kotlin/days/day04/Day4.kt
+++ src/main/kotlin/days/day04/Day4.kt
@@ -10,14 +10,8 @@ class Day4(override val input: String) : Day<Int>(input) {
 
 	override fun solve1(): Int = matrix.mapMatrixIndexed { point, c ->
 		if (c != 'X') 0
-		else check(point)
+		else checkDirections(point)
 	}.matrixSumOf { it }
-
-	private fun check(p: Point): Int = Point.directions.fold(0) { acc, dir ->
-		val letters = (1..3).map { matrix.getOrElse(p + (dir * it), '.') }.joinToString("")
-		if (letters == "MAS") acc + 1
-		else acc
-	}
 
 	override fun solve2(): Int {
 		var count = 0
@@ -32,10 +26,21 @@ class Day4(override val input: String) : Day<Int>(input) {
 		return count
 	}
 
+	private fun checkDirections(p: Point): Int = Point.directions.fold(0) { acc, dir ->
+		val letters = getLine(p, dir, 1..3, matrix)
+		if (letters == "MAS") acc + 1
+		else acc
+	}
+
 	private fun getDiagonals(subMatrix: Matrix<Char>): Pair<String, String> {
 		val topLeft = Point(0, 0)
 		val topRight = Point(2, 0)
-		fun getDiagonal(start: Point, direction: Point) = (1..3).map { subMatrix[start + (direction * it)] }.joinToString("")
+		fun getDiagonal(start: Point, direction: Point) = getLine(start, direction, 0..2, subMatrix)
 		return getDiagonal(topLeft, Point.DOWN_RIGHT) to getDiagonal(topRight, Point.DOWN_LEFT)
 	}
+
+	private fun getLine(start: Point, dir: Point, range: IntRange, matrix: Matrix<Char>) = range
+		.map { matrix.getOrElse(start + (dir * it), '.') }
+		.joinToString("")
+
 }
