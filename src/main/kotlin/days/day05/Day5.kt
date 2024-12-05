ab1eacf5f3f33dd161fba77b1ab84f841b489353
ckainz11 <christoph.kainz11@gmail.com>
2024-12-05 10:12:07 +0100

    solve day 5

---
+++ src/main/kotlin/days/day05/Day5.kt
@@ -0,0 +1,37 @@
+package days.day05
+
+import days.Day
+import util.allInts
+import util.middle
+import util.pairwise
+import util.sections
+
+class Day5(override val input: String) : Day<Int>(input) {
+
+	private val sections = input.sections()
+	private val rules = sections[0].lines().map { it.allInts().let { (a, b) -> a to b } }
+	private val updates = sections[1].lines().map { it.allInts() }
+
+	override fun solve1(): Int = updates
+		.filter { it.isValid() }
+		.sumOf { it.middle() }
+
+	override fun solve2() = updates
+		.filter { !it.isValid() }
+		.map { it.toItemList().sorted() }
+		.sumOf { it.middle().item }
+
+	private fun List<Int>.isValid() = pairwise().all { it in rules }
+	private fun List<Int>.toItemList() = map { Item(it, rules) }
+
+	data class Item(val item: Int, val rules: List<Pair<Int, Int>>) : Comparable<Item> {
+
+		override fun compareTo(other: Item): Int {
+			if (item == other.item) return 0
+			val entry = rules.firstOrNull { it.first == item && it.second == other.item }
+			return if (entry != null) 1 else -1
+		}
+
+	}
+
+}
--- src/main/kotlin/util/DayFactory.kt
+++ src/main/kotlin/util/DayFactory.kt
@@ -5,15 +5,17 @@ import days.day01.Day1
 import days.day02.Day2
 import days.day03.Day3
 import days.day04.Day4
+import days.day05.Day5
 
 class DayFactory {
 	companion object {
 
-		val days = mapOf(
+		private val days = mapOf(
 			1 to ::Day1,
 			2 to ::Day2,
 			3 to ::Day3,
-			4 to ::Day4
+			4 to ::Day4,
+			5 to ::Day5
 		)
 
 		fun getDayObject(day: Int, input: String): Day<out Any> {
