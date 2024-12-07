c25947530f1da8d5311e0890d34c0d9852a25c23
ckainz11 <christoph.kainz11@gmail.com>
2024-12-07 14:34:47 +0100

    refactor(day7): extract part logic

--- src/main/kotlin/days/day07/Day7.kt
+++ src/main/kotlin/days/day07/Day7.kt
@@ -3,36 +3,34 @@ package days.day07
 import days.Day
 import util.allLongs
 
-class Day7(override val input: String) : Day<Long>(input) {
+fun Long.concat(other: Long) = "$this$other".toLong()
 
-	private val equations = input.lines().map { line -> Equation.fromString(line) }
+class Day7(override val input: String) : Day<Long>(input) {
 
 	override fun solve1(): Long = calculateCalibration(part = 1)
 	override fun solve2(): Long = calculateCalibration(part = 2)
 
-	private fun calculateCalibration(part: Int) = equations
-		.filter { it.solvable(part) }
+	private fun calculateCalibration(part: Int) = input.lines()
+		.map { Equation.fromString(it, part) }
+		.filter { it.solvable() }
 		.sumOf { it.result }
 
-	data class Equation(val result: Long, val numbers: List<Long>) {
+	data class Equation(val result: Long, val numbers: List<Long>, val part: Int) {
 		companion object {
 
-			fun fromString(line: String) = line.allLongs().let { Equation(it.first(), it.drop(1)) }
+			fun fromString(line: String, part: Int) = line.allLongs().let { Equation(it.first(), it.drop(1), part) }
 
-			private val operations = listOf(
-				{ a: Long, b: Long -> a + b },
-				{ a: Long, b: Long -> a * b },
-			)
+			private val operations = listOf<(Long, Long) -> Long>(Long::plus, Long::times)
+			private val part2Operations = operations + Long::concat
 
-			private val part2Operations = operations + { a: Long, b: Long -> "$a$b".toLong() }
+			fun operators(part: Int) = if (part == 1) operations else part2Operations
 		}
 
-		fun solvable(part: Int, actual: Long = this.numbers.first(), numbers: List<Long> = this.numbers.drop(1)): Boolean {
+		fun solvable(actual: Long = this.numbers.first(), numbers: List<Long> = this.numbers.drop(1)): Boolean {
 			if (numbers.isEmpty()) return actual == result
 			if (actual > result) return false
 
-			return if (part == 1) operations.any { solvable(part, it(actual, numbers.first()), numbers.drop(1)) }
-			else part2Operations.any { solvable(part, it(actual, numbers.first()), numbers.drop(1)) }
+			return operators(part).any { solvable(it(actual, numbers.first()), numbers.drop(1)) }
 		}
 
 	}
