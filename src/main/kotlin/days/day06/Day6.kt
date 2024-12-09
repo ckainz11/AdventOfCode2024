c3e3ae4d1f2fdcd2a44bf89ecbc2c626885069fe
ckainz11 <christoph.kainz11@gmail.com>
2024-12-09 20:20:47 +0100

    refactor day 6

--- src/main/kotlin/days/day06/Day6.kt
+++ src/main/kotlin/days/day06/Day6.kt
@@ -7,10 +7,16 @@ class Day6(override val input: String) : Day<Int>(input) {
 
 	private val grid = input.lines().map { it.toCharArray().toList() }
 	private val guardStart = grid.matrixIndexOfFirst { it == '^' }
+	private val path = getPath()
 
-	override fun solve1(): Int = walk(grid) + 1
+	override fun solve1(): Int = path.size
 
-	private fun walk(grid: Matrix<Char>, partTwo: Boolean = false): Int {
+	override fun solve2(): Int = path.sumOf { p ->
+		if (p == guardStart) 0
+		else walk(putObstacle(p))
+	}
+
+	private fun walk(grid: Matrix<Char>): Int {
 		var dir = Point.UP
 		var pos = guardStart
 		val visited = mutableSetOf<Pair<Point, Point>>()
@@ -20,18 +26,26 @@ class Day6(override val input: String) : Day<Int>(input) {
 				dir = dir.rotateClockwise()
 			pos += dir
 		}
-		return if (!partTwo) visited.map { it.first }.distinct().size
-		else if (pos to dir in visited) 1 else 0
+		return if (pos to dir in visited) 1 else 0
 	}
 
-	override fun solve2(): Int = grid.mapMatrixIndexed { point, _ ->
-		if (point == guardStart) 0
-		else walk(replace(point, '#'), true)
-	}.matrixSumOf { it }
+	private fun getPath(): Set<Point> {
+		var dir = Point.UP
+		var pos = guardStart
+		val path = mutableSetOf<Point>()
+		while (pos + dir inBoundsOf grid) {
+			path += pos
+			while (grid[pos + dir] == '#')
+				dir = dir.rotateClockwise()
+			pos += dir
+		}
+		path += pos
+		return path
+	}
 
-	private fun replace(p: Point, c: Char): Matrix<Char> {
+	private fun putObstacle(p: Point): Matrix<Char> {
 		val new = grid.toMutableMatrix()
-		new[p] = c
+		new[p] = '#'
 		return new
 	}
 }
