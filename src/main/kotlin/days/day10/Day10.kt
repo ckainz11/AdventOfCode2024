0476932efb7c59341c0b697869b1f8e61654f620
ckainz11 <christoph.kainz11@gmail.com>
2024-12-10 09:57:11 +0100

    refactor day 10

--- src/main/kotlin/days/day10/Day10.kt
+++ src/main/kotlin/days/day10/Day10.kt
@@ -8,28 +8,17 @@ class Day10(override val input: String) : Day<Int>(input) {
 	private val grid = input.asMatrix().mapMatrix { it.digitToInt() }
 	private val trailheads = grid.mapMatrixIndexedNotNull { p, c -> if (c == 0) p else null }
 
-	override fun solve1(): Int = trailheads.sumOf { score(it).size }
-	override fun solve2(): Int = trailheads.sumOf { score2(it) }
+	override fun solve1(): Int = trailheads.sumOf { it.calcScore() }
+	override fun solve2(): Int = trailheads.sumOf { it.calcRating() }
 
-	private fun score(trailhead: Point): Set<Point> {
-		if (grid[trailhead] == 9) return setOf(trailhead)
-		val visitedEnds = mutableSetOf<Point>()
-		val neighbors = trailhead.cardinalNeighbors()
-		for (neighbor in neighbors) {
-			if (neighbor inBoundsOf grid && grid[neighbor] == grid[trailhead] + 1)
-				visitedEnds += score(neighbor)
-		}
-		return visitedEnds
-	}
+	private fun Point.calcScore() = calc(this).distinct().size
+	private fun Point.calcRating() = calc(this).size
 
-	private fun score2(trailhead: Point): Int {
-		if (grid[trailhead] == 9) return 1
-		var score = 0
+	private fun calc(trailhead: Point): List<Point> {
+		if (grid[trailhead] == 9) return listOf(trailhead)
 		val neighbors = trailhead.cardinalNeighbors()
-		for (neighbor in neighbors) {
-			if (neighbor inBoundsOf grid && grid[neighbor] == grid[trailhead] + 1)
-				score += score2(neighbor)
-		}
-		return score
+			.filter { it inBoundsOf grid && grid[it] == grid[trailhead] + 1 }
+		if (neighbors.isEmpty()) return emptyList()
+		return neighbors.flatMap { calc(it) }
 	}
 }
