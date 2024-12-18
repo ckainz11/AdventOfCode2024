ee6cd526ef225637e86797de72c747734e366026
ckainz11 <christoph.kainz11@gmail.com>
2024-12-18 16:55:05 +0100

    refactor(day18): use abstracted binary search util

--- src/main/kotlin/days/day18/Day18.kt
+++ src/main/kotlin/days/day18/Day18.kt
@@ -23,21 +23,11 @@ class Day18(override val input: String) : Day<String>(input) {
 	override fun solve1(): String = ImplicitGraph.withStartNode(startNode)
 		.shortestPathToOrNull(end)?.toString() ?: "No path found"
 
-	override fun solve2(): String {
-		var low = 0
-		var high = bytes.size - 1
-		while (low < high) {
-			val mid = low + (high - low) / 2
-
-			val fallenBytes = bytes.take(mid).toSet()
-			val startNode = Node(Point(0, 0), 0) { it inBoundsOf grid && it !in fallenBytes }
-			val found = ImplicitGraph.withStartNode(startNode).shortestPathToOrNull(end) == null
-
-			if (found) high = mid
-			else low = mid + 1
-		}
-		return bytes[low - 1].let { "${it.x},${it.y}" }
-	}
+	override fun solve2(): String = binarySearchLowerBound(0, bytes.size - 1) { index ->
+		val fallenBytes = bytes.take(index).toSet()
+		val startNode = Node(Point(0, 0), 0) { it inBoundsOf grid && it !in fallenBytes }
+		ImplicitGraph.withStartNode(startNode).shortestPathToOrNull(end) == null
+	}.let { bytes[it - 1] }.let { "${it.x},${it.y}" }
 
 	data class Node(val position: Point, override var distance: Int, private val valid: ((Point) -> Boolean)) : ImplicitNode<Point, Node> {
 
