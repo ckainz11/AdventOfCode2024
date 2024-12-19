e25871b46288841fdf19b5e568805e76c191b105
ckainz11 <christoph.kainz11@gmail.com>
2024-12-19 17:10:17 +0100

    refactor(day19): use num of possibilities to determine if design is possible

--- src/main/kotlin/days/day19/Day19.kt
+++ src/main/kotlin/days/day19/Day19.kt
@@ -8,28 +8,13 @@ class Day19(override val input: String) : Day<Long>(input) {
 	private val availableTowels = input.sections()[0].split(", ").toSet()
 	private val designs = input.sections()[1].lines()
 
-	private val possibleCache = mutableMapOf<String, Boolean>()
-	private val possibleDesigns = mutableListOf<String>()
+	override fun solve1(): Long = designs.count { it.countPossibilities() > 0 }.toLong()
+	override fun solve2(): Long = designs.sumOf { it.countPossibilities() }
 
-	override fun solve1(): Long = designs
-		.filter { it.isPossible() }
-		.also { possibleDesigns.addAll(it) }
-		.size.toLong()
-
-	override fun solve2(): Long = possibleDesigns.sumOf { it.countPossibilities() }
-
-	private fun String.isPossible(): Boolean =
-		isEmpty() ||
+	private val cache = mutableMapOf("" to 1L)
+	private fun String.countPossibilities(): Long = cache.getOrPut(this) {
 		availableTowels
 			.filter { startsWith(it) }
-			.map { removePrefix(it) }
-			.any { subDesign -> possibleCache.getOrPut(subDesign) { subDesign.isPossible() } }
-
-	private val countCache = mutableMapOf<String, Long>()
-	private fun String.countPossibilities(): Long =
-		if (isEmpty()) 1
-		else availableTowels
-			.filter { startsWith(it) }
-			.map { removePrefix(it) }
-			.sumOf { subDesign -> countCache.getOrPut(subDesign) { subDesign.countPossibilities() } }
+			.sumOf { removePrefix(it).countPossibilities() }
+	}
 }
