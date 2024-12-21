d978d8e3b8d82984e7210c3ed8bee9eed7d60572
ckainz11 <christoph.kainz11@gmail.com>
2024-12-21 19:31:33 +0100

    feat(collectionUtil): add permutation and join utils

--- src/main/kotlin/util/CollectionUtils.kt
+++ src/main/kotlin/util/CollectionUtils.kt
@@ -11,3 +11,19 @@ fun <T> List<T>.middle(): T {
 	if (size % 2 == 0) error("List must have an odd number of elements")
 	return this[size / 2]
 }
+
+/*----- permutations -----*/
+
+fun <T> List<T>.permutations(): List<List<T>> {
+	return if (this.size == 1) listOf(this)
+	else this.flatMap { i -> (this - i).permutations().map { listOf(i) + it } }
+}
+
+/*----- String Joining -----*/
+
+fun <T> Array<T>.join() = this.joinToString("")
+fun <T> Array<T>.join(separator: String) = this.joinToString(separator)
+fun CharArray.join() = this.joinToString("")
+fun CharArray.join(separator: String) = this.joinToString(separator)
+fun <T> Iterable<T>.join() = this.joinToString("")
+fun <T> Iterable<T>.join(separator: String) = this.joinToString(separator)
